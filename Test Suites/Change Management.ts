b8c1ffc24207da2965b217d17cff33f71910d445
anu.nv <anu.nv@NVANU.experionglobal.local>
2023-02-02 17:50:19 +0530

    New test suite

---
+++ Test Suites/Change Management.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Change Management</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e641f52c-f44c-49a2-b962-2688939a935e</testSuiteGuid>
+</TestSuiteEntity>
